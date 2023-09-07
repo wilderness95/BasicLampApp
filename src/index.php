79cce83c80afa88f195537b20fc52370af47b3c7
Zoltan Horvath <47075392+wilderness95@users.noreply.github.com>
2023-09-07 23:07:03 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,8 +1,6 @@
 <?php
-// Include the database connection file
 include_once("config.php");
 
-// Fetch contacts (in descending order)
 $result = mysqli_query($mysqli, "SELECT * FROM contacts ORDER BY id DESC"); 
 ?>
 <html>
