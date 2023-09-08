f1e83264058af2257300bbcc7b44be1b23d40994
Zoltan Horvath <47075392+wilderness95@users.noreply.github.com>
2023-09-08 14:05:54 +0200

    Update versionupdater.sh

--- versionupdater.sh
+++ versionupdater.sh
@@ -9,3 +9,4 @@ IFS='.' read -ra version_parts <<< "$version"
 new_version=$(IFS='.'; echo "${version_parts[*]}" | tr -d ' ')
 
 echo "wilderness95/phpcontactapp:$new_version" > version.txt
+sed -i "s/PHP_TAG=.*/PHP_TAG=$new_version/g" .env
